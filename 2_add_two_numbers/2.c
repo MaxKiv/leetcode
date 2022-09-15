59ff36d887554252d5ff8999fd19d6d3b10f6103
Max Kivits <maxkivits42@gmail.com>
2022-09-15 23:05:45 +0200

    further work on 2

--- 2_add_two_numbers/2.c
+++ 2_add_two_numbers/2.c
@@ -16,19 +16,24 @@ struct ListNode* addTwoNumbers(struct ListNode* l1, struct ListNode* l2) {
   int combined[101] = {-1};
   struct ListNode* l1_og = l1;
   struct ListNode* out = l1;
-  for(int i = 0; l1->next != NULL || l2->next != NULL; i++){
+  int i = 0;
+  for(;l1->next != NULL && l2->next != NULL; i++){
     combined[i] = l1->val + l2->val;
     l1 = l1->next;
     l2 = l2->next;
   }
-  max_print(combined, 101, sizeof(int));
 
-  for(int i = 0; i < 100; i++) {
-    if(combined[i] >= 10) {
-      combined[i+1] += 1;
+  printf("size of list found: %d\n", i);
+  max_print(combined, i, sizeof(int));
+
+  for(int j = 0; j < i; j++) {
+    //TODO why is combined[i] = 6 and why is size 1 found?
+    printf("combined[i] = %d ", combined[j]);
+    if(combined[j] >= 10) {
+      combined[j+1] += 1;
     }
-    printf("combined[i] = %d", combined[i]);
-    l1_og->val = combined[i];
+    printf("Inserting into out: %d\n", combined[j]);
+    l1_og->val = combined[j];
     l1_og = l1_og->next;
   }
   return out;
